adb2707f23c35bc17afb0cad033c531f487d2d08
pagulreact <145957108+pagulreact@users.noreply.github.com>
2023-09-25 22:28:43 -0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,101 @@
+
+
+
+// const days=["mon", "tue","wed","thur"];
+// const d=new Date("2023-08-16");
+
+// let day=days[d.getDay()];
+//  document.getElementById=demo.innerhtml=day
+// ---------------------------------------------------------------------------------
+// let x=10;
+// let y=20;
+// // sum1=x*y;
+// sum=x+y;
+// // console.log(sum1);
+// console.log(sum);
+// -------------------------------------------------------------------
+// var arr1=[1,3,5,8,]
+// var arr2=[]
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7];
+// const array3=[1,3,5]
+
+// const equalArr=[]//push values of third array that are same in both array1 and array2
+
+// for(a3 of array3)
+// {
+//     if(array1.includes(a3) && array2.includes(a3))
+//     {
+//         equalArr.push(a3)
+//     }
+// }
+
+// console.log(equalArr);
+
+
+
+
+
+// const array3 = [3, 4,5];
+
+// Method 1: Using nested loops
+// const commonValuesNested = [];
+// for (const value of array3) {
+//   if (array1.includes(value) && array2.includes(value)) {
+//     commonValuesNested.push(value);
+//   }
+// }
+// console.log(commonValuesNested);
+
+// Method 2: Using built-in array methods (filter)
+// const commonValuesFilter = array3.filter(value => array1.includes(value) && array2.includes(value));
+// console.log(commonValuesFilter);
+
+// Method 3: Using Set intersection
+// const set1 = new Set(array1);
+// const set2 = new Set(array2);
+// const commonValuesSet = array3.filter(value => set1.has(value) && set2.has(value));
+// console.log(commonValuesSet);
+// -----------------------------------------------------------------------------------------------------
+//
+
+// const arr=[]
+
+// for (let i=1;i<=100;i++){
+//     if(i%6==0  &&  i%9==0){
+//         arr.push(i)
+//     }
+// }
+// console.log(
+    // =============================================================
+    
+// let str="Welcome All"
+
+// const arr=str.split("")
+
+// // console.log(arr);
+// const newArr=arr;
+// let t=newArr[0]
+// newArr[0]=newArr[newArr.length-1]
+// newArr[newArr.length-1]=t
+
+// console.log(newArr.join(''));
+
+
+  
+
+////////////////////////////////////////////////////////////////////////////////
+
+//arrt=[2,3,2,5,1]
+
+//console.log(arry);
+
+
+
+
+
+
+
+
+
+
